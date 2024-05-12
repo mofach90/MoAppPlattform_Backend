d22f1541122da77085f03039b74ef105ba34675f
Mohamed Islem Ayari <mohamed-islem.ayari@cloudpilots.com>
2024-05-12 16:37:49 +0200

    Add test envirenment

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,20 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || (process.env.NODE_ENV === 'test' ? 4000 : 3000);
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("Welcome to MoAppBackend ");
 });
 
-app.listen(port, () => {
+app.get("/test",(_,res)=>{
+  res.set('Content-Type','text/html')
+  res.status(200).send("<h1>success status 200</h1>")
+})
+
+const server = app.listen(port, () => {
   console.log(`server running at http:\\localhost:${port}`);
 });
+
+
+export {app,server}
---
+++ src/test/app.test.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+import {app, server} from "../server";
+
+describe("first test ", () => {
+  it("test the endpoint /test", async () => {
+    const response = await request(app)
+    .get('/test')
+    .expect('Content-Type', /text\/html/)
+    .expect(200)
+    expect(response.text).toEqual("<h1>success status 200</h1>")
+})});
+
+afterAll((done) => {
+    server.close(done);
+  });
