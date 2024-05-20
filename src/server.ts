11b31e15a664eb263d926815347fdeed3bf0e965
Mohamed Islem Ayari <mohamed-islem.ayari@cloudpilots.com>
2024-05-20 17:17:48 +0200

    Add formbasedauth setup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import "dotenv/config";
 import express from "express";
+import bodyParser from "body-parser"
+import { formBasedAuth } from "./middleware/formBasedAuthentication";
 
 const app = express();
 const port = process.env.PORT || (process.env.NODE_ENV === 'test' ? 4000 : 3000);
 
+app.use(bodyParser.json());
+
 app.get("/", (_, res) => {
   res.send("Welcome to MoAppBackend ");
 });
+
+app.use("/login", formBasedAuth)
 
 app.get("/test",(_,res)=>{
   res.set('Content-Type','text/html')
