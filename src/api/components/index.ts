2153e2b45d0a7f1de073622114d029e18038e5e2
mofach90 <ayari.mohamed@web.de>
2024-07-23 20:23:16 +0200

    Add check auth middleware

--- src/api/components/index.ts
+++ src/api/components/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createAuthRoutes } from './Auth/routes';
 import createTasksRoutes from './Tasks/routes';
+import { checkAuthforApps } from '../../services/auth/checkAuthforApps';
 
 export function registerApiRoutes(router: Router, prefix: string = ''): void {
   router.use(`${prefix}/auth`, createAuthRoutes());
-  router.use(`${prefix}/tasks`, createTasksRoutes());
+  router.use(`${prefix}/tasks`, checkAuthforApps ,createTasksRoutes());
 }
